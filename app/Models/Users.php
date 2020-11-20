4d762b7bc573f3a2cc135c9a7f3d17eb5080230c
vagrant <vagrant@homestead>
2020-11-20 22:02:30 +0000

    Initial commit

---
+++ app/Models/Users.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Users extends Model
+{
+    
+    protected $primaryKey = 'user_id';
+    // use HasFactory;
+}
